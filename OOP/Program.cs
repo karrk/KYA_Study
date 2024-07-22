1d8eb11cb966d3a3ebd590c4e89869ab45b5cd41
karrk <101902544+karrk@users.noreply.github.com>
2024-07-22 13:21:48 +0900

    init
    init

---
+++ OOP/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    //과제 1.
+    //Chracter
+    //클래스 선언하기
+    //Chracter 클래스를 선언하고, 클래스가 가져야 할 필드와 메서드는 아래와 같다.
+    //필드
+    //레벨
+    //체력
+    //이동 속도
+    //공격력
+    //메서드
+    //전진
+    //후진
+    //좌회전(90도)
+    //우회전(90도)
+    //공격
+    //피격
+    //제출 형태
+    //C# 콘솔 프로젝트
+    //개인 과제용 Git Repository 디렉토리 생성 후 Push
+    //디렉토리 이름 : 내 제출일 6자(Ex> 240619)
+
+    //과제 2. 몬스터와 플레이어 클래스 선언하기
+    //과제 1의 Character를 상속받는 Monster, PlayerCharacter 클래스 생성하기
+    //플레이어와 몬스터는 필드를 개별적으로 변경할 수 있어야 한다.
+
+    //과제 3. 몬스터와 플레이어를 관리하는
+    //CharacterManager
+    //클래스 생성하기
+    //몬스터와 플레이어를 하나의 타입으로 관리할 수 있는 CharacterManager 클래스를 선언한다.
+    //이 때, Main 함수 내에서 CharacterManager의 인스턴스를 생성하고, 몬스터와 플레이어의 행동을 관리할 수 있어야 한다.
+}
